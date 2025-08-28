5d6811f1eb038f3590cd45bc0e27a3b69812d2d3
Adrian <adrian52507@gmail.com>
2025-08-28 16:55:06 -0500

    raqueta0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import Background from "three/src/renderers/common/Background.js";
 
-function Box() {
+function Racket() {
   return (
-    <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="royalblue" />
-    </mesh>
+    <group rotation={[-0.5, 0, 0]} position={[0, 0, 0]}>
+      {/* Cabeza de la raqueta */}
+      <mesh position={[0, 0.5, 0]}>
+        <boxGeometry args={[0.6, 1.2, 0.05]} />
+        <meshStandardMaterial color="green" />
+      </mesh>
+      {/* Mango de la raqueta */}
+      <mesh position={[0, -0.5, 0]}>
+        <cylinderGeometry args={[0.1, 0.1, 1, 32]} />
+        <meshStandardMaterial color="lime" />
+      </mesh>
+    </group>
   );
 }
 
 export default function App() {
   return (
-    <Canvas style={{ height: "100vh", width: "100vw", background: "black" }}>
+    <Canvas
+      style={{ height: "100vh", width: "100vw", background: "black" }}
+    >
       <ambientLight intensity={0.5} />
       <directionalLight position={[2, 2, 2]} />
-      <Box />
+      <Racket />
       <OrbitControls />
     </Canvas>
   );
